b0417837c6a09075354402440dae2fbe603571ce
nayemdeshipay <nayem@deshipay.com>
2022-11-02 20:58:15 +0600

    have to design navbar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return(
+        <div className="bg-[#4fb65c]">
+            <button>Cookups</button>
+            <input type="text" />
+            <button>categories</button>
+            <button>cart</button>
+            <button>user</button>
+        </div>
+    )
+}
+
+export default NavBar;
